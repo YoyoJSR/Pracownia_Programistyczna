de9100ee06117501afbc85be78ed48363aa02fb1
YoyoJSR <akiraxq@gmail.com>
2026-02-28 12:01:01 +0100

    Create lab1.c

---
+++ lab1.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+int main(){
+/*
+    int liczba1;
+    int liczba2;
+    int liczba3;
+    printf("podaj 1 liczbe: \n");
+    scanf("%d",&liczba1);
+    printf("podaj 2 liczbe: \n");
+    scanf("%d",&liczba2);
+    printf("podaj 3 liczbe: \n");
+    scanf("%d",&liczba3);
+    float srednia = (liczba1+liczba2+liczba3)/3;
+    printf("srednia twoich liczb: %f",srednia);
+    */
+
+    //zadanie 5
+    /*
+    char znak1;
+    char znak2;
+    printf("podaj 1 znak: ");
+    scanf("%c",&znak1);
+    printf("podaj 2 znak: ");
+    scanf(" %c",&znak2);
+    printf("%c %c",znak2,znak1);
+    */
+
+    //zadanie 6
+    /*
+    float liczba;
+    printf("podaj liczbe: ");
+    scanf("%f",&liczba);
+    printf("%f",liczba*2);
+    */
+
+    //zadanie 7
+    /*
+    float kwota;
+    printf("podaj kwote w dolarach: ");
+    scanf("%f",&kwota);
+    float euro = (kwota * 0.85);
+    printf("%.2lf",euro);
+    */
+
+    //Zadanie 8
+    //printf("to jest cytat: \"Czesto uzywam jezyka C\" ");
+
+    //Zadanie 9
+    //printf("C:\\\\Program Files\\\\MojaAplikacja\\\\");
+
+
+    //Zadanie 10
+    //printf("Specjalne znaki: \\t (tabulacja), \\n (nowa linia), %% (procent), \\\\ (ukosnik wsteczny).");
+
+    //Zadanie 11
+    /*
+    double liczba1 =  3.0/4.0;
+    double liczba2 = 8.0/7.0;
+    double przeciwprostakatna = sqrt((liczba1,2) + pow(liczba2,2));
+    printf("dlugosc przeciwprostokatnej: %f",przeciwprostakatna);
+    */
+
+    //zadanie 11
+    
+
+
+    return 0;
+}
