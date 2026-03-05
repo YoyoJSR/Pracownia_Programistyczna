d161574e951dca2646ec7ab02c7669d4d88c8666
Aleksander <akiraxq@gmail.com>
2026-03-05 19:36:31 +0100

    Create operatory_arytmetyczne.c

---
+++ operatory_arytmetyczne.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+/*
+
+    //Zadanie1
+    int liczba1 = 3;
+    int liczba2 = 4;
+    int liczba3 = 4;
+    int reszta =  (liczba1 + liczba2)%liczba3;
+    printf("reszta z dzielenia wynosi: %d \n",reszta);
+
+    //Zadanie 2
+    int kwadrat = pow(liczba1,2) - pow(liczba2,2);
+    printf("roznica kwadratow 2 liczb wynosi: %d\n",kwadrat);
+
+
+    //zadanie 3
+    int roznica = liczba1 - liczba2;
+    int iloczyn = roznica * liczba3;
+    printf("wynik: %d \n",iloczyn);
+
+
+    //Zadanie 4
+    double geometric_mean = pow(abs(liczba1*liczba2*liczba3),1.0/3);
+    printf("srednia geometryczna 3 liczb wynosi: %g\n",geometric_mean);
+
+    //Zadanie 5
+    kwadrat = pow(liczba1+liczba2,2);
+    printf("kwadrat sumy 2 liczb wynosi: %d\n",kwadrat);
+
+    //Zadanie 6
+    int kwadrat2 = (pow(liczba1,2) + pow(liczba2,2) + pow(liczba3,2));
+    printf("suma kwadratow 3 liczb wynosi: %d\n",kwadrat2);
+    */
+
+    //Zadanie 7
+    /*
+    int liczba1=5;
+    int liczba2 = 4;
+    int licznik = liczba1/liczba2;
+    printf("Liczba %d miesci sie w %d: %d razy",liczba2,liczba1,licznik);
+    */
+
+    //Zadanie 8
+    /*
+    int liczba1 = 6;
+    int liczba2 = 8;
+    int roznica = pow(liczba1,2) - pow(liczba2,2);
+    printf("Wynik: %d",roznica);
+    */
+
+    //Zadanie 9
+    /*
+    int liczba1 = 10;
+    int liczba2 = 5;
+    int suma = liczba1+liczba2;
+    int roznica = liczba1-liczba2;
+    int iloraz = suma/roznica;
+    printf("wynik: %d",iloraz);
+    */
+
+
+    //Zadanie 10
+    /*
+    int liczba1=2;
+    int liczba2=liczba1 +1;
+    int liczba3=liczba2+1;
+    printf("Suma liczb: %d",liczba1+liczba2+liczba3);
+    */
+
+    //Zadanie 11
+    /*
+    float liczba1 = 3;
+    float liczba2 = 7;
+    float liczba3 = 31;
+    float srednia = (liczba1+liczba2+liczba3)/3;
+    printf("srednia arytmetyczna wynosi: %.2f",srednia);
+    */
+
+    //Zadanie 12
+    /*
+    float a = 2;
+    float b = 3;
+    float c = 4;
+    float wzor = 1/(1/a+1/b+1/c); 
+    printf("wynik: %.2f",wzor);
+    */
+
+    //Zadanie 13
+
+    return 0;
+}
+
